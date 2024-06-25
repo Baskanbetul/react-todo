6f04bc079fb06cb33d0c07d528bdff14cccef3db
Baskanbetul <btlbskn@hotmail.com>
2024-06-25 00:36:33 -0600

    Made a new file for TodoList and moved list into new todoList file

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import React from "react";
+import TodoList from "./TodoList";
 
-const todoList = [
-  { title: "Go shopping", id: "1" },
-  { title: "Finish cleaning", id: "2" },
-  { title: "Go to gym", id: "3" },
-];
+// const todoList = [
+//   { title: "Go shopping", id: "1" },
+//   { title: "Finish cleaning", id: "2" },
+//   { title: "Go to gym", id: "3" },
+// ];
 
-function TodoList() {
-  return (
-    <ul>
-      {todoList.map(function (item) {
-        return (
-          <li key={item.objectID}>
-            <span>{item.title}</span>
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
+// function TodoList() {
+//   return (
+//     <ul>
+//       {todoList.map(function (item) {
+//         return (
+//           <li key={item.objectID}>
+//             <span>{item.title}</span>
+//           </li>
+//         );
+//       })}
+//     </ul>
+//   );
+// }
 
 function Search() {
   return (
