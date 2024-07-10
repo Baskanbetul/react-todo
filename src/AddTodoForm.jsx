9e73064fc789219bf05fb4784ecea0f58ff02dca
Baskanbetul <btlbskn@hotmail.com>
2024-07-09 19:18:26 -0600

    Made new file named on TodoListItem and added new attribute , function named on handleAddTodo

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 
 function AddTodoForm() {
+  function handleAddTodo(event) {
+    event.preventDefault();
+    console.log("Form Submitted");
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={handleAddTodo}>
         <label htmlFor="title" style={{ margin: "5px" }}>
           Title
         </label>
-        <input id="todoTitle" type="text"></input>
-        <button>Add</button>
+        <input
+          name="title"
+          placeholder="todo"
+          id="todoTitle"
+          type="text"
+        ></input>
+        <button type="submit">Submit</button>
       </form>
 
       {/* <label htmlFor="search">Search: </label> */}
