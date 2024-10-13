88d035dd27dcf51f2ee46dae96dee3c4d11ea954
Baskanbetul <btlbskn@hotmail.com>
2024-10-12 18:04:06 -0600

    Added CSS Styled Components SVG, changed background color, used Media Queries to make sure the application is responsive for all device sizes

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import style from "./TodoListItem.module.css";
 
 function TodoListItem({ title, id, onRemoveTodo }) {
   return (
-    <li>
+    <li className={`${style.listItem} ${style.ListItem}`}>
       <span>{title}</span>
       <button onClick={() => onRemoveTodo(id)}>Remove</button>
     </li>
